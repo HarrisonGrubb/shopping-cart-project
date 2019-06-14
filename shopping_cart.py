f0e144982ae9d8175adbc5f3f81c3edfeade6356
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-13 23:05:10 -0400

    starting email receipt process

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,5 +1,15 @@
 import datetime
 import pandas as pd
+from dotenv import load_dotenv
+import sendgrid
+from sendgrid.helpers.mail import * # source of Email, Content, Mail, etc.
+
+load_dotenv()
+
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+MY_EMAIL_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
+
+
 
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
